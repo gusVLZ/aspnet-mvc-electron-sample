285379659f8907899dfcd88e5cdc164190e012bd
Velozo, Gustavo <gustavo.velozo@inwave.com.br>
2023-02-25 22:00:29 -0300

    Add project files.

---
+++ Be/Data/BancoDados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Be.Models;
+
+namespace Be.Data
+{
+    public class BancoDados : DbContext
+    {
+        public BancoDados (DbContextOptions<BancoDados> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Be.Models.Filme> Filme { get; set; } = default!;
+    }
+}
---
+++ Be/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Be.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Nome do filme")]
+        public string Nome { get; set; }
+        [DisplayName("Sinopse do filme")]
+        public string Descricao { get; set; }
+        [DisplayName("Ano de lançamento")]
+        public int AnoLancamento { get; set; }
+    }
+}
